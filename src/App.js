daa047fb0a6b4b9a6bc2ecf9c5bc17e90a01b130
Lakshay <nijhawanlakshay06@gmail.com>
2023-05-29 18:22:12 +0530

    Commit to load project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import News from './components/News';
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<NavBar/>} />
+        </Routes>
+        <Routes>
+          <Route exact path='/' element={<News />} />
+        </Routes>
+      </Router>
+    )
+  }
+}
+
