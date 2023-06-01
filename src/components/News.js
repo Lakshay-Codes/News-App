380733d3cf90d7af4c333352931b3f90cc4fc61a
Lakshay <nijhawanlakshay06@gmail.com>
2023-06-01 23:18:50 +0530

    Added images in news app

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,19 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-4">
+        <h2>News App - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my desc" imageUrl="https://images.bild.de/64775f3a201bb848421a0515/89eb4ed60d39e1cd55186e5a10356857,54184a86?w=1280" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my desc" imageUrl="https://images.bild.de/64775f3a201bb848421a0515/89eb4ed60d39e1cd55186e5a10356857,54184a86?w=1280" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my desc" imageUrl="https://images.bild.de/64775f3a201bb848421a0515/89eb4ed60d39e1cd55186e5a10356857,54184a86?w=1280" />
+          </div>
+        </div>
       </div>
     )
   }
