754de0f6165e8fc30342ef4a91ba5cb426f126e5
Lakshay <nijhawanlakshay06@gmail.com>
2023-06-02 22:31:24 +0530

    Added fetch data from api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    const { title, description, imageUrl} = this.props;
+    const { title, description, imageUrl,newsUrl} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={imageUrl? imageUrl : "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/news" className="btn btn-sm btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title? title : ""}</h5>
+            <p className="card-text">{description?description : ""}</p>
+            <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">
+              Read more
             </a>
           </div>
         </div>
