b08f7618af4208ab48e827a88a54b8d7c5ea7211
Lakshay <nijhawanlakshay06@gmail.com>
2023-06-04 22:08:11 +0530

    Added Top Loading Bar

--- src/components/News.js
+++ src/components/News.js
@@ -12,7 +12,7 @@ export default class News extends Component {
       articles: [],
       loading: true,
       page: 1,
-      totalResults: 0
+      totalResults: 0,
     };
     document.title =
       "News App-" + this.capitalizeFirstLetter(this.props.category);
@@ -29,7 +29,7 @@ export default class News extends Component {
   };
   fetchMoreData = async () => {
     this.setState({
-      page: this.state.page+1
+      page: this.state.page + 1,
     });
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
     let data = await fetch(url);
@@ -37,13 +37,13 @@ export default class News extends Component {
     this.setState({
       articles: this.state.articles.concat(ParsedData.articles),
       totalResults: ParsedData.totalResults,
-      loading: false 
+      loading: false,
     });
   };
   capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  
+
   async updatePage() {
     this.props.setProgress(20);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
@@ -54,14 +54,14 @@ export default class News extends Component {
     this.setState({
       articles: ParsedData.articles,
       totalResults: ParsedData.totalResults,
-      loading: false
+      loading: false,
     });
     this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updatePage();
     this.setState({
-      page: this.state.page+1
+      page: this.state.page + 1,
     });
   }
 
@@ -77,29 +77,33 @@ export default class News extends Component {
           <InfiniteScroll
             dataLength={this.state.articles.length}
             next={this.fetchMoreData}
-            hasMore={this.state.articles.length<this.state.totalResults}
-            loader={(this.props.category==='general')? <h1> </h1> : <Spinner />}
+            hasMore={this.state.articles.length < this.state.totalResults}
+            loader={
+              this.props.category === "general" ? <h1> </h1> : <Spinner />
+            }
           >
-            <div className="container row">
-              {this.state.articles.map((element) => {
-                return (
-                  <div key={element.url} className="col-md-4">
-                    <NewsItem
-                      title={element.title}
-                      description={element.description}
-                      imageUrl={element.urlToImage}
-                      newsUrl={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                      source={element.source}
-                    />
-                  </div>
-                );
-              })}
+            <div className="container ">
+              <div className="row">
+                {this.state.articles.map((element) => {
+                  return (
+                    <div key={element.url} className="col-md-4">
+                      <NewsItem
+                        title={element.title}
+                        description={element.description}
+                        imageUrl={element.urlToImage}
+                        newsUrl={element.url}
+                        author={element.author}
+                        date={element.publishedAt}
+                        source={element.source}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </InfiniteScroll>
         }
-      </ >
+      </>
     );
   }
 }
